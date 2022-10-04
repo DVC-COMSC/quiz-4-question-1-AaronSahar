9fc653ec60ee281af876d31bdd2d76caa8b10973
Aaron Sahar <aaronsahar@gmail.com>
2022-10-03 22:10:21 -0700

    test

--- q1.py
+++ q1.py
@@ -1,9 +1,9 @@
 numbers = []
 highest = 0
 cluster = 0
-numbers.append(int(input("Enter an integer")))
-for i in range(9):
+for i in range(10):
     numbers.append(int(input("Enter an integer")))
-    if (numbers[i] % 2 == 1) and (numbers[i-1] % 2 == 0):
-        cluster += 1
+    if i>0:
+        if (numbers[i] % 2 == 1) and (numbers[i-1] % 2 == 0):
+            cluster += 1
 print(cluster)
