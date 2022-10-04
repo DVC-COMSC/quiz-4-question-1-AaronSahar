8e65345d4e26bd8d6b79d9adf66624ee9289f2b9
Aaron Sahar <aaronsahar@gmail.com>
2022-10-03 21:50:48 -0700

    test

--- q1.py
+++ q1.py
@@ -8,3 +8,13 @@
 # No need to use list
 # All input values are taken one by one separatively.
 ###
+
+numbers = []
+cluster = 1
+numbers.append(int(input("Enter an integer")))
+i=1
+for i in range(9):
+    numbers.append(int(input("Enter an integer")))
+    if (numbers[i] % 2 == 0) and (numbers[i-1] % 2 == 0):
+        cluster += 1
+print(cluster)
