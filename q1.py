0b2cd2d0867160c3cf69ae14210453330eeaedbe
Aaron Sahar <aaronsahar@gmail.com>
2022-10-04 01:37:42 -0700

    test

--- q1.py
+++ q1.py
@@ -2,10 +2,7 @@ numbers = []
 cluster = 0
 for i in range(10):
     numbers.append(int(input("Enter an integer")))
-    if i==0:
-        if numbers[i] % 2 == 0:
-            cluster += 1
-    if i>0:
-        if numbers[i] % 2 == 0 and numbers[i-1] % 2 == 1:
+    if i>1:
+        if numbers[i] % 2 == 1 and numbers[i-1] % 2 == 0 and numbers[i-2] % 2 == 0:
             cluster += 1
 print(cluster)
